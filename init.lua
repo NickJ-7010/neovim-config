3b7fac0cb5b67d5eab83f4cef318e194fa60cc48
NickJ-7010 <nickj7010@gmail.com>
2025-10-17 14:05:47 -0400

    Resolved small merge conflict and fix other bugs

--- init.lua
+++ init.lua
@@ -54,6 +54,9 @@ local configs = {
             "xml",
             "yaml"
         }
+    },
+    oil = {
+        default_file_explorer = false
     }
 }
 
@@ -68,7 +71,9 @@ require("lazy").setup({
     { "folke/snacks.nvim", priority = 999, lazy = false, opts = configs.snacks },
     { 'nvim-mini/mini.pairs', version = '*', opts = {} },
     { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }, opts = {} },
-    { "nvim-tree/nvim-tree.lua", version = "*", dependencies = { "nvim-tree/nvim-web-devicons" }, opts = {} }
+    { "nvim-tree/nvim-tree.lua", version = "*", dependencies = { "nvim-tree/nvim-web-devicons" }, opts = {} },
+    { "ThePrimeagen/harpoon", dependencies = { 'nvim-lua/plenary.nvim' }, opts = {} },
+    { 'stevearc/oil.nvim', dependencies = { "nvim-tree/nvim-web-devicons" }, opts = configs.oil }
 })
 
 require("typescript-tools").setup({})
@@ -84,7 +89,7 @@ vim.cmd.colorscheme "vscode"
 vim.o.background = 'dark'
 
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = ","
 
 vim.opt.cursorline = true
 vim.opt.number = true
@@ -92,6 +97,35 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.wrap = false
+
+local harpoon = require("harpoon.ui");
+vim.keymap.set('n', 'ha', require("harpoon.mark").add_file, { desc = 'Harpoon mark file' })
+vim.keymap.set('n', 'hm', harpoon.toggle_quick_menu, { desc = 'Harpoon open menu' })
+
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', telescope.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', telescope.help_tags, { desc = 'Telescope help tags' })
+
+local function open_nvim_tree(data)
+
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 local function open_nvim_tree(data)
 
